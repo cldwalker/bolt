15dcfe8568aae18cf141601074aed8f1027a053a
Gabriel Horner <gabriel.horner@gmail.com>
2014-01-14 10:37:53 -0500

    autofocus search + remove cruft from freebase app

--- src/bolt/core.cljs
+++ src/bolt/core.cljs
@@ -23,39 +23,8 @@
   (om/component
    (dom/form #js {:onSubmit #(submit-search chan %)
                   :className "jumbotron"}
-            (dom/input #js {:type "text" :id "search_term"})
+            (dom/input #js {:type "text" :id "search_term" :autoFocus "autofocus"})
             (dom/input #js {:type "submit" :value "Search"}))))
-
-;; TODO: give each element a :key
-(defn render-table [headers rows]
-  (apply dom/table
-         #js {:className "table table-striped"}
-         nil
-         (dom/caption nil (str "Found " (count rows) " results"))
-         (apply dom/tr nil
-                  (map
-                   (fn [value] (dom/th nil value))
-                   headers))
-         (map #(apply dom/tr
-                       nil
-                       (map
-                        (fn [value] (dom/td nil value))
-                        %))
-               rows)))
-
-(defn click-id-link [chan id e]
-  (.log js/console e)
-  (put! chan [:service.id id])
-  false)
-
-(defn search-results [app owner {:keys [chan]}]
-  (om/component
-   (dom/div
-    #js {:id "search_results"}
-    (if-let [result (:search-result app)]
-      (do (.log js/console "DATA" result)
-          (render-table ["h1" "h2"] [["c1" "c2"] ["d1" "d2"]]))
-      ""))))
 
 (defn process-search
   "For now this is just a local config lookup but this could interact
