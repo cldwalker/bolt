0c872dedb5259ee46fe9b3d692d6b44fde5919ff
Gabriel Horner <gabriel.horner@gmail.com>
2014-01-30 08:56:06 -0500

    some useful cmds

--- src/bolt/config.cljs
+++ src/bolt/config.cljs
@@ -13,4 +13,18 @@
     :wikipedia {:alias "w" :url "http://en.wikipedia.org/w/index.php?search=%s&fulltext=Search"}
     :gmail {:alias "gmas" :url "https://mail.google.com/mail/u/0/?shva=1#search/%s"}
     :wolfram {:alias "wram" :url "http://www.wolframalpha.com/input/?i=%s"}
-    :ltgtfy {:alias "gfun" :url "http://lmgtfy.com/?q=%s"}}})
+    :ltgtfy {:alias "gfun" :url "http://lmgtfy.com/?q=%s"}
+    :lastfm {:url "http://www.last.fm/search?q=%s"}
+    :clojuredocs {:alias "cd" :url "http://clojuredocs.org/search?q=%s"}
+    :flickr {:alias "fl" :url "http://www.flickr.com/search/?q=%s"}
+
+    ;; just for voice
+    :github {:alias "gh" :url "https://github.com/search?q=%s&ref=simplesearch"}
+    :yelp {:alias "y" :url "http://www.yelp.com/search?find_desc=%s&src=opensearch"}
+    :clojars {:alias "cl" :url "http://clojars.org/search?q=%s"}
+
+    ;; for local dev
+    :semtag-search {:alias "ss" :url "http://localhost:8000/semtag-web-dev.html#/search?query=%s"}
+    :semtag-regex-search {:alias "sr" :url "http://localhost:8000/semtag-web-dev.html#/search?query=%s&search-type=regex"}
+    :semtag-type {:alias "st" :url "http://localhost:8000/semtag-web-dev.html#/type/%s"}
+    :semtag-thing {:alias "se" :url "http://localhost:8000/semtag-web-dev.html#/thing/%s"}}})
