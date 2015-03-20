9d3028e4d9480ee794e3c895184b08f9db76bb22
Gabriel Horner <gabriel.horner@gmail.com>
2015-03-20 09:57:06 -0400

    Basic search working. search-result isn't in app state

--- src/bolt/core.cljs
+++ src/bolt/core.cljs
@@ -1,21 +1,13 @@
 (ns bolt.core
   (:require
-   [rum]
    [cljs.core.async :refer [put! <! >! chan timeout]]
-   [bolt.config :as config])
+   [bolt.config :as config]
+   [clojure.string :as string]
+   [rum])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (def app-state (atom {}))
 (def event-ch (chan 10))
-
-(defn handle-event [app event event-data]
-  (.log js/console "Event: " (pr-str event event-data))
-  ;; (case event
-  ;;   :service.search (process-search event-data chan)
-  ;;   :ui.search  (om/update! app assoc :search-result event-data)
-  ;;   (.log js/console "No event found for" event event-data))
-  )
-
 (def commands-index
   (memoize (fn []
              (merge
@@ -27,6 +19,51 @@
                     (filter :alias)
                     (map (fn [cmd] [(keyword (:alias cmd)) (:url cmd)]))
                     (into {}))))))
+
+;; Event handling
+
+(defn process-search*
+  "For now this is just a local config lookup but this could interact
+  with another service."
+  [input]
+  ;; decode to use as a default search engine
+  (let [[cmd & args] (-> input js/decodeURIComponent (string/split #"\s+"))
+        url (get (commands-index) (keyword cmd))
+        [cmd url args] (if (and (nil? url) (:default config/config))
+                         [(:default config/config)
+                          (get (commands-index) (:default config/config))
+                          (into [cmd] args)]
+                         [cmd url args])]
+    {:name cmd :url url :args args}))
+
+(defn process-search
+  [event-data ch]
+  (put! ch [:ui.search (process-search* event-data)]))
+
+(defn build-url [url args]
+  (let [expected-args (count (re-seq #"%s" url))]
+    (cond
+      (= 1 expected-args) (string/replace-first url "%s" (string/join " " args))
+      ;; TODO: error for non-matching args
+      :else (apply string/replace url "%s" args))))
+
+(defn handle-search-result [app {:keys [url] :as cmd}]
+  (if url
+    ;; TODO: url encode args
+    (let [redirect-url (build-url url (:args cmd))]
+      (swap! app assoc :message
+             (string/replace-first "Redirecting with %s ..." "%s" (name (:name cmd))))
+      (set! (.-location js/window) redirect-url))
+    (swap! app assoc :error (str "No command found for " (:name cmd)))))
+
+(defn handle-event [app event event-data]
+  (.log js/console "Event: " (pr-str event event-data))
+  (case event
+    :service.search (process-search event-data event-ch)
+    :ui.search      (handle-search-result app event-data)
+    (.log js/console "No event found for" event event-data)))
+
+;; UI
 
 (defn submit-search [e]
   (put! event-ch [:service.search
