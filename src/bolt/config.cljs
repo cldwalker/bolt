1288cf62ddda94384207f076c3bb28b7d24b674f
Gabriel Horner <gabriel.horner@gmail.com>
2015-04-12 13:35:16 -0400

    Add history command

--- src/bolt/config.cljs
+++ src/bolt/config.cljs
@@ -32,6 +32,7 @@
     :drive {:alias "dr" :url "https://drive.google.com/drive/search?q=%s"}
     :bus {:url "http://openmbta.org/routes/Bus#%s"}
     :fandango {:alias "fan" :url "http://www.fandango.com/search/?q=%s"}
+    :history {:url "https://history.google.com/history/find?q=%s"}
 
     ;; groups
     :film {:names ["fandango" "imdb" "rotten"]}
